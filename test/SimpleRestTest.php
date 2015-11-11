b3dcb6dd3a5d5d52715511b8255f444e9c86fa79
Rohan Sakhale <rs@saiashirwad.com>
2015-11-11 23:22:57 +0530

    Minor change

---
+++ test/SimpleRestTest.php
@@ -0,0 +1,16 @@
+<?php
+use sailabs\SimpleRest;
+
+class SimpleRestTest extends PHPUnit_Framework_TestCase
+{
+    public function testRESTGet()
+    {
+        $simpleRest = new SimpleRest();
+        $usersMeta = $simpleRest->get('http://tools.saiashirwad.com/api/users/meta');
+        $this->assertCount(3, $usersMeta, 'Simple REST Service response will always be of size 3 elements');
+        $this->assertArrayHasKey('response', $usersMeta, '`response` key is required as it contains the HTTP Response Body');
+        $this->assertArrayHasKey('status_code', $usersMeta, '`status_code` key is required as it contains the HTTP Status Code');
+        $this->assertArrayHasKey('response_headers', $usersMeta, '`response_headers` key is required as it contains the HTTP Response Headers');
+        $this->assertEquals(200, $usersMeta['status_code'], '`200` means HTTP invocation was successful');
+    }
+}
