06e8da706cb7c633cdc0a8d68c51aacde7db38a6
Rohan Sakhale <rs@saiashirwad.com>
2015-11-12 09:49:58 +0530

    Update Test Structure

--- test/sailabs/test/SimpleRestTest.php
+++ test/sailabs/test/SimpleRestTest.php
@@ -1,12 +1,20 @@
 <?php
+namespace sailabs\test;
+
 use sailabs\SimpleRest;
 
-class SimpleRestTest extends PHPUnit_Framework_TestCase
+class SimpleRestTest extends \PHPUnit_Framework_TestCase
 {
+
+    public $simpleRest;
+
+    public function setUp()
+    {
+        $this->simpleRest = new SimpleRest();
+    }
     public function testRESTGet()
     {
-        $simpleRest = new SimpleRest();
-        $usersMeta = $simpleRest->get('http://tools.saiashirwad.com/api/users/meta');
+        $usersMeta = $this->simpleRest->get('http://tools.saiashirwad.com/api/users/meta');
         $this->assertCount(3, $usersMeta, 'Simple REST Service response will always be of size 3 elements');
         $this->assertArrayHasKey('response', $usersMeta, '`response` key is required as it contains the HTTP Response Body');
         $this->assertArrayHasKey('status_code', $usersMeta, '`status_code` key is required as it contains the HTTP Status Code');
